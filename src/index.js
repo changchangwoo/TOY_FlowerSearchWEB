05614e6d293d7ec171b6affd3e0e05bfc11d7960
changchangwoo <changchangwoo@gmail.com>
2024-01-24 16:03:35 +0900

    feat : SPA 프로젝트 구조

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import App from "./App.js"
+import Nav from "./Nav.js"
+// import Footer from "./Footer.js"
+
+
+new Nav({ $target: document.querySelector(".Nav")});
+new App({ $target: document.querySelector(".App")});
+// new Footer({ $target: document.querySelector(".Footer")});
