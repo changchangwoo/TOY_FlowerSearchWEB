a087bde4c0516af48dc6b0129856657238f30670
changchangwoo <changchangwoo@gmail.com>
2024-01-31 19:54:47 +0900

    feat : DetailSectionAPI 통신 구현, DarkMode 구현중

--- src/Nav.js
+++ src/Nav.js
@@ -2,6 +2,10 @@ export default class Nav {
     constructor({ $target }) {
         this.$target = $target
         this.render()
+        let $darkButton = document.querySelector('.darkButton')
+        this.$darkButton = $darkButton
+
+        $darkButton.addEventListener('click', e => this.changeMode(e))
     }
 
     render = () => {
@@ -9,5 +13,22 @@ export default class Nav {
         <div class="navTitle textMedium">초록 <span>소리</span> </div>
         <div class="darkButton"></div>
         </div>`
+    }
+
+    changeMode = (e) => {
+        let list = []
+        let $App = document.querySelector(".App")
+        let $body = document.querySelector("body")
+        let $searchSection = document.querySelector(".searchSection");
+        let $contentsSection = document.querySelector(".contentsSection");
+        list.push($body, $App, $searchSection, $contentsSection)
+        
+        for(let i = 0; i < list.length; i++) {
+            if(list[i].classList.contains('darkMode')) {
+                list[i].classList.remove('darkMode')
+            } else {
+                list[i].classList.add('darkMode')
+            }
+        }
     }
 }
--- src/components/detailSection.js
+++ src/components/detailSection.js
@@ -1,9 +1,68 @@
 export default class DetailSection {
-    constructor({$target, $detailData}) {
+    
+    data = []
 
+    constructor({$target, $detailData}) {
+        this.$target = $target
+        this.data = $detailData
+        this.visible = false
+        const $detailSection = document.createElement('div');
+        $detailSection.className = "detailSection"
+        this.$detailSection = $detailSection
+
+        $detailSection.addEventListener("click", e => this.exitDetail(e))
+        this.$target.appendChild($detailSection)
+        this.render()
     }
 
     render = () => {
+        let $body = document.querySelector("body")
+        if($body.classList.contains('darkMode')) {
+            this.$detailSection.classList.add('darkMode')
+        } else {
+            if(this.$detailSection.classList.contains('darkMode')) {
+            this.$detailSection.classList.remove('darkMode')
+            }
+            return
+        }
+        if(this.visible && this.data.imgUrl === "NONE") {
+            
+            this.$detailSection.style.display = "flex";
+            this.$detailSection.innerHTML = `
+            <div class="detailContainer">
+            <div class="detailName textLarge">${this.data.plantGnrlNm}</div>
+            <div class="detailEng textSmall">${this.data.engNm}</div>
+            <div class="detailSub textSmall">${this.data.familyKorNm}</div>
+            <div class="detaillNullBox"><span>이미지준비중</span></div>
+            <div class="detailDesc textSmall">반갑습니다.</div>
+            </div>`
+        } else if(this.visible && this.data.imgUrl !== "NONE") {
+            this.$detailSection.style.display = "flex";
+            this.$detailSection.innerHTML = `
+            <div class="detailContainer">
+            <div class="detailName textLarge">${this.data.plantGnrlNm}</div>
+            <div class="detailEng textSmall">${this.data.engNm}</div>
+            <div class="detailSub textSmall">${this.data.familyKorNm}</div>
+            <img src="${this.data.imgUrl}"/>
+            <div class="detailDesc textSmall">반갑습니다.</div>
+            </div>`
+
+        }
+    }
+
+    setState(nextData) {
+        console.log(nextData)
+        this.data = nextData.data[0];
+        this.visible = nextData.visible;
+        this.render();
+    }
+
+    exitDetail = (e) => {
+        const clickedElement = e.target;
+        if(clickedElement.className === "detailSection") {
+            this.$detailSection.style.display = "none"
+            this.$detailSection.innerHTML = ``
+        }
 
     }
 
