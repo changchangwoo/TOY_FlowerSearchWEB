a599d2d36d8ac73a5e9a6f6bb835f740c850ef89
changchangwoo <changchangwoo@naver.com>
2024-02-01 20:02:37 +0900

    fix : 프로토콜 수정2

--- src/components/api.js
+++ src/components/api.js
@@ -1,4 +1,4 @@
-const url = '//openapi.nature.go.kr/openapi/service/rest/PlantService/';
+const url = 'https://openapi.nature.go.kr/openapi/service/rest/PlantService/';
 const serviceKey = 'kHnTn7oETYUtW1eKWdJn8Go%2B6AQppJLnxRtxb3rn4JmDxLYB9bvufX6M6rDhGeiNEyajV2ADU82A%2B%2Bu1WnpSNA%3D%3D';
 
 const fetchDataList = async (keyword) => {
