cc2d2953c312041c0b90da8003b608041f531758
changchangwoo <changchangwoo@gmail.com>
2024-02-01 19:01:51 +0900

    feat : DarkMode 구현, 반응형 디자인(모바일) 구현, DetailSection 설명 추가

--- src/components/detailSection.js
+++ src/components/detailSection.js
@@ -1,32 +1,22 @@
 export default class DetailSection {
-    
-    data = []
 
-    constructor({$target, $detailData}) {
+    data = []
+    desc = ''
+
+    constructor({ $target, $detailData }) {
         this.$target = $target
-        this.data = $detailData
-        this.visible = false
+        this.data = $detailData.visible
+        this.visible = $detailData.visible
         const $detailSection = document.createElement('div');
         $detailSection.className = "detailSection"
         this.$detailSection = $detailSection
-
         $detailSection.addEventListener("click", e => this.exitDetail(e))
         this.$target.appendChild($detailSection)
         this.render()
     }
 
     render = () => {
-        let $body = document.querySelector("body")
-        if($body.classList.contains('darkMode')) {
-            this.$detailSection.classList.add('darkMode')
-        } else {
-            if(this.$detailSection.classList.contains('darkMode')) {
-            this.$detailSection.classList.remove('darkMode')
-            }
-            return
-        }
-        if(this.visible && this.data.imgUrl === "NONE") {
-            
+        if (this.visible && this.data.imgUrl === "NONE") {
             this.$detailSection.style.display = "flex";
             this.$detailSection.innerHTML = `
             <div class="detailContainer">
@@ -34,9 +24,9 @@ export default class DetailSection {
             <div class="detailEng textSmall">${this.data.engNm}</div>
             <div class="detailSub textSmall">${this.data.familyKorNm}</div>
             <div class="detaillNullBox"><span>이미지준비중</span></div>
-            <div class="detailDesc textSmall">반갑습니다.</div>
+            <div class="detailDesc textSmall">${this.desc}</div>
             </div>`
-        } else if(this.visible && this.data.imgUrl !== "NONE") {
+        } else if (this.visible && this.data.imgUrl !== "NONE") {
             this.$detailSection.style.display = "flex";
             this.$detailSection.innerHTML = `
             <div class="detailContainer">
@@ -44,9 +34,18 @@ export default class DetailSection {
             <div class="detailEng textSmall">${this.data.engNm}</div>
             <div class="detailSub textSmall">${this.data.familyKorNm}</div>
             <img src="${this.data.imgUrl}"/>
-            <div class="detailDesc textSmall">반갑습니다.</div>
+            <div class="detailDesc textSmall">${this.desc}</div>
             </div>`
-
+        }
+        let $body = document.querySelector("body")
+        let $detailContainer = document.querySelector('.detailContainer')
+        console.log($body.classList.contains('darkMode'), $detailContainer)
+        if ($body.classList.contains('darkMode')) {
+            if ($detailContainer) $detailContainer.classList.add('darkMode')
+        } else {
+            if ($detailContainer && $detailContainer.classList.contains('darkMode')) {
+                $detailContainer.classList.remove('darkMode')
+            }
         }
     }
 
@@ -54,12 +53,18 @@ export default class DetailSection {
         console.log(nextData)
         this.data = nextData.data[0];
         this.visible = nextData.visible;
+        this.desc = ''
+        let descArr = [this.data.flwrDesc, this.data.fritDesc, this.data.shpe]
+        for(let i = 0; i < descArr.length; i++) {
+            this.desc = this.desc + descArr[i] + "\n"
+        }
+        console.log(this.desc)
         this.render();
     }
 
     exitDetail = (e) => {
         const clickedElement = e.target;
-        if(clickedElement.className === "detailSection") {
+        if (clickedElement.className === "detailSection") {
             this.$detailSection.style.display = "none"
             this.$detailSection.innerHTML = ``
         }
